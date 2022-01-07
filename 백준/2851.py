aed843c75a323bf382a9f37a8863f0a27c442787
Zho <chlwlgh1011@naver.com>
2022-01-08 00:05:37 +0900

    done

--- 백준/2851.py
+++ 백준/2851.py
@@ -1,5 +1,13 @@
 n = []
-
+check = 0
+check1 = 0
 for i in range(10):
     n.append(int(input()))
-print(n)
+    check += n[i]
+    if check >= 100:
+        check1 = check - n[i]
+        break
+if (check - 100 <= 100 - check1):
+    print(check)
+else:
+    print(check1)
