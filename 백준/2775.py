dcd7b5e135e6f5afd099817d97b92448658c531f
Zho <chlwlgh1011@naver.com>
2022-11-19 17:40:56 +0100

    부녀회장와우

--- 백준/2775.py
+++ 백준/2775.py
@@ -1,12 +1,10 @@
 case = int(input())
 
-for i in case:
-    a = int(input())
-    b = int(input())
-    people = 1
-    while 0:
-        if a == 0:
-            break;
-        for i in a:
-            for j in b:
-                
+for _ in range(case):
+    floor = int(input())
+    ho = int(input())
+    f_0 = [x for x in range(1, ho+1)]
+    for _ in range(floor):
+        for i in range(1, ho):
+            f_0[i] += f_0[i-1]
+    print(f_0[-1])
