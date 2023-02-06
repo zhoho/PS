6d707228c971c75775fecd0ac71ff5e182f2e4db
Zho <chlwlgh1011@naver.com>
2023-02-06 21:45:59 +0900

    dfs remind

--- 백준/6603.py
+++ 백준/6603.py
@@ -1,5 +1,20 @@
-while 0:
+def dfs(start, depth):
+    if depth == 6:
+        for i in range(6):
+            print(answer[i], end=' ')
+        print()
+        return
+    for i in range(start, len(S)):
+        answer[depth] = S[i]
+        dfs(i + 1, depth + 1)
+
+
+answer = [0 for i in range(13)]
+while 1:
     S = list(map(int, input().split()))
     K = S[0]
     if K == 0:
         break
+    del S[0]
+    dfs(0, 0)
+    print()
