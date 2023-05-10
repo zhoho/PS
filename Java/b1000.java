12127be7be105dd8fc1d37205953531ca7e6ed5d
Zho <chlwlgh1011@naver.com>
2023-05-10 22:52:04 +0900

    myfirstJavasolve

---
+++ Java/b1000.java
@@ -0,0 +1,16 @@
+package Java;
+
+import java.util.Scanner;
+
+public class b1000 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int A = in.nextInt();
+        int B = in.nextInt();
+
+        System.out.println(A + B);
+
+        in.close();
+
+    }
+}
