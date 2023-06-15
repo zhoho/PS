c18493757bf551b5080c70580769a557b4385602
Zho <chlwlgh1011@naver.com>
2023-06-15 22:09:51 +0900

    recursivecall

---
+++ Java/recursivecall.java
@@ -0,0 +1,18 @@
+public static int numberOfZeros(int n)
+{
+    if(n == 0)
+    {
+        return 1;
+    }
+    else if(n < 10)
+    {
+        return 0;
+    }
+    else if(n%10 ==0)
+    {
+        return(numberOfZeros(n/10)+1);
+    }
+    else{
+        return(numberOfZeros(n/10));
+    }
+}
