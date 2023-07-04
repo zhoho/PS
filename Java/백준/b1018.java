7347187c6a1fffb613f622d5b67a542f6683040c
Zho <chlwlgh1011@naver.com>
2023-07-04 22:23:18 +0900

    b1018하다말다

---
+++ Java/백준/b1018.java
@@ -0,0 +1,14 @@
+package 백준;
+
+import java.io.*;
+import java.util.*;
+
+public class b1018 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] inputs = br.readLine().split(" ");
+
+        int m = Integer.parseInt(inputs[0]);
+        int n = Integer.parseInt(inputs[1]);
+    }
+}
