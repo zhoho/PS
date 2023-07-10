c476d7f9b3600a8ffd884a3ee156fb2d4040d4b2
Zho <chlwlgh1011@naver.com>
2023-07-10 21:51:23 +0900

    1

---
+++ Java/백준/b1422.java
@@ -0,0 +1,16 @@
+package 백준;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class b1422 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] inputs = br.readLine().split(" ");
+
+        int n = Integer.parseInt(inputs[0]);
+        int m = Integer.parseInt(inputs[1]);
+        
+    }   
+}
