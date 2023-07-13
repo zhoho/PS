6a0fa677d8d6bb346958730037c6e2e39304b991
Zho <chlwlgh1011@naver.com>
2023-07-13 11:41:08 +0900

    b033

---
+++ Java/백준/pps/b033.java
@@ -0,0 +1,31 @@
+package 백준.pps;
+
+import java.util.Scanner;
+
+public class b033 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int T, value, ori, A = 300, B = 60, C = 10;
+        int[] ans = new int[3];
+        T = in.nextInt();
+        ori = T;
+        T = (T % A);
+        value = (ori / A);
+        ans[0] = value;
+
+        ori = T;
+        T = (T % B);
+        value = (ori / B);
+        ans[1] = value;
+
+        ori = T;
+        T = (T % C);
+        value = (ori / C);
+        ans[2] = value;
+
+        if (T != 0)
+            System.out.println("-1");
+        else
+            System.out.println(ans[0] + " " + ans[1] + " " + ans[2]);
+    }
+}
