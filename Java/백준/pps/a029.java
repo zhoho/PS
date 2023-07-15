80e742c230c411b48976e3201e010a3043ebeb0e
Zho <chlwlgh1011@naver.com>
2023-07-15 23:02:45 +0900

    029done

---
+++ Java/백준/pps/a029.java
@@ -0,0 +1,25 @@
+package 백준.pps;
+
+import java.util.Scanner;
+
+public class a029 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int a = in.nextInt();
+
+        if (n > 5) {
+            System.out.println("Love is open door");
+        } else {
+            for (int i = 0; i < n - 1; i++) {
+                if (a == 1) {
+                    System.out.println(0);
+                    a = 0;
+                } else {
+                    System.out.println(1);
+                    a = 1;
+                }
+            }
+        }
+    }
+}
