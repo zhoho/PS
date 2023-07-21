0fa6c4603eaab9bca168ed74dc7f921681a77da5
Zho <chlwlgh1011@naver.com>
2023-07-21 11:16:07 +0900

    7/21solve

---
+++ Java/pps/week1/week2/b036.java
@@ -0,0 +1,30 @@
+package pps.week1.week2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class b036 {
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+        int sum = Integer.MAX_VALUE;
+        StringTokenizer st = new StringTokenizer(in.readLine(), "-");
+
+        while (st.hasMoreTokens()) {
+            int tmp = 0;
+            StringTokenizer addst = new StringTokenizer(st.nextToken(), "+");
+
+            while (addst.hasMoreTokens()) {
+                tmp += Integer.parseInt(addst.nextToken());
+            }
+            if (sum == Integer.MAX_VALUE) {
+                sum = tmp;
+            } else {
+                sum -= tmp;
+            }
+        }
+        System.out.println(sum);
+    }
+}
