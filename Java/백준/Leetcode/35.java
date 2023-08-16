37510c58ba24c3cbe0db1b598b6e710bcb114bc7
Zho <chlwlgh1011@naver.com>
2023-08-16 10:17:13 +0900

    0816_solve

--- Java/백준/Leetcode/35.java
+++ Java/백준/Leetcode/35.java
@@ -1,5 +1,13 @@
 class Solution {
     public int searchInsert(int[] nums, int target) {
-
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == target)
+                return i;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > target)
+                return i;
+        }
+        return nums.length;
     }
 }
