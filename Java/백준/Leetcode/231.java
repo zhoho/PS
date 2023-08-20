d4b31cf500ef682fc5fd97283c6143ab71284bbd
Zho <chlwlgh1011@naver.com>
2023-08-20 12:16:00 +0900

    0820_solve

---
+++ Java/백준/Leetcode/231.java
@@ -0,0 +1,9 @@
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        for (int i = 0; i < n; i++) {
+            if (Math.pow(2, i) == n)
+                return true;
+        }
+        return false;
+    }
+}
