65195fde32648485356c4a2f664490198a09df8f
Zho <chlwlgh1011@naver.com>
2023-08-31 22:37:22 +0900

    ja

---
+++ Java/clinic/a001.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Main stepA = new Main();
+        stepA.a01();
+    }
+
+    void a01() {
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter a birth year > ");
+        int birthyear = s.nextInt();
+        int age = 2022 - birthyear;
+        System.out.println("You are " + age + " years old.");
+    }
+}
