0eb63f4092d5144a042f662bbd335d5eeabf1f59
Zho <chlwlgh1011@naver.com>
2023-09-11 20:49:11 +0900

    0911_solve

---
+++ Java/백준/Leetcode/121.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int min = prices[0];
+        int max = 0;
+
+        for (int i = 0; i < prices.length; i++) {
+            max = Math.max(max, prices[i] - min);
+            min = Math.min(prices[i], min);
+        }
+        return max;
+    }
+}
