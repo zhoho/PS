a8ba531b562169da0d675f21b634a4548f6345e8
Zho <chlwlgh1011@naver.com>
2023-10-08 19:40:31 +0900

    1008_solve

---
+++ Java/leetcode/1592.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isThree(int n) {
+        int cnt = 0;
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                cnt++;
+            }
+        }
+        if (cnt == 3)
+            return true;
+        return false;
+    }
+}
