0afeb8e3e1689f421df918fe22d89cda464f3837
zho <71474242+zhoho@users.noreply.github.com>
2024-08-14 16:06:10 +0900

    [Bronze III] Title: 전자레인지, Time: 248 ms, Memory: 18716 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/10162. 전자레인지/전자레인지.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int T, value, ori, A = 300, B = 60, C = 10;
+        int[] ans = new int[3];
+        T = in.nextInt();
+        ori = T;
+        T = (T % A);
+        value = (ori / A);
+        ans[0] = value;
+
+        ori = T;
+        T = (T % B);
+        value = (ori / B);
+        ans[1] = value;
+
+        ori = T;
+        T = (T % C);
+        value = (ori / C);
+        ans[2] = value;
+
+        if (T != 0)
+            System.out.println("-1");
+        else
+            System.out.println(ans[0] + " " + ans[1] + " " + ans[2]);
+    }
+}
