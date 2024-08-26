cdf331b1b306fe850253fba6fda05de48fc81a7e
zho <71474242+zhoho@users.noreply.github.com>
2024-08-26 17:08:04 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 8.17 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.py
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.py
@@ -1,5 +1,4 @@
 def solution(s):
-    answer = []
     total_count = 0
     total_zerocount = 0
     while(len(s) != 1):
@@ -8,15 +7,9 @@ def solution(s):
         for i in s:
             if i == '0':
                 zero_count += 1
-        # print(zero_count)
         total_zerocount += zero_count
         s = exchange(len(s) - zero_count)
-        # print('s = '+s)
-    answer.append(total_count)
-    answer.append(total_zerocount)
-
-    return answer
-
+    return [total_count, total_zerocount]
 
 def exchange(n):
     result = bin(n)[2:]
