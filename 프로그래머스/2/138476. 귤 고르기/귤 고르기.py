e6bffcd77fa60e73d2858a91ffe52fee049692f8
zho <71474242+zhoho@users.noreply.github.com>
2024-09-04 22:25:53 +0900

    Update 귤 고르기.py
    풀이 하나 추가

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.py
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.py
@@ -1,15 +1,33 @@
-from collections import Counter
-
-### 풀이 1 - Counter 이용
+### 풀이 2 dict 사용, 
+## 핵심 key 
+## a = dict(sorted(a.items(), key = lambda x:x[1], reverse = True))
 def solution(k, tangerine):
-    result = 0
     answer = 0
-    
-    temp = Counter(tangerine)
-    temp = temp.most_common()
-    
-    for i in temp:
-        result += i[1]
-        answer += 1
-        if(result >= k):
+    dic = {}
+    for i in tangerine:
+        if i in dic:
+            dic[i] += 1
+        else:
+            dic[i] = 1
+    dic = dict(sorted(dic.items(), key = lambda x:x[1], reverse = True))    
+    for i in dic:
+        if k <= 0:
             return answer
+        k-=dic[i]
+        answer+=1        
+    return answer
+# from collections import Counter
+
+# ### 풀이 1 - Counter 이용
+# def solution(k, tangerine):
+#     result = 0
+#     answer = 0
+    
+#     temp = Counter(tangerine)
+#     temp = temp.most_common()
+    
+#     for i in temp:
+#         result += i[1]
+#         answer += 1
+#         if(result >= k):
+#             return answer
