01f4265b1e30fd149456e19c1a9121c16a86c71a
zho <71474242+zhoho@users.noreply.github.com>
2024-11-12 21:23:11 +0900

    [Silver V] Title: 색종이, Time: 96 ms, Memory: 109240 KB -BaekjoonHub

---
+++ 백준/Silver/2563. 색종이/색종이.py
@@ -0,0 +1,10 @@
+n = int(input())
+paper = [[0] * 100 for _ in range(100)]
+for _ in range(n):
+    x, y = map(int, input().split())
+    for i in range(x, x + 10):
+        for j in range(y, y + 10):
+            paper[i][j] = 1
+
+ans = sum(sum(row) for row in paper)
+print(ans)
