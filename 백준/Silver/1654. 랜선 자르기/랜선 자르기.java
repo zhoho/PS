e906036d18451afe4ed797b1fb5913c45bd2f1ac
zho <71474242+zhoho@users.noreply.github.com>
2025-03-27 21:48:18 +0900

    [Silver II] Title: 랜선 자르기, Time: 116 ms, Memory: 15432 KB -BaekjoonHub

---
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		st = new StringTokenizer(br.readLine());
+
+        int k = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[k];
+        long max = 0;
+
+        for (int i = 0; i < k; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+			if(max < arr[i]) {
+				max = arr[i];
+			}
+		}
+
+        long left = 1;
+        long right = max;
+        long answer = 0;
+
+        while (left <= right) {
+            long mid = (left + right) / 2;
+            long count = 0;
+
+            for (int i = 0; i < k; i++) {
+                count += (arr[i] / mid);
+            }
+
+            if (count >= n) {
+                answer = mid;
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        System.out.println(answer);
+    }
+}
